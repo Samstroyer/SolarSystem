52804c2bd1c66e9e8d12aab74699547160a07ec0
Samuel Palmer <palmer.samuel@elev.ga.ntig.se>
2023-02-05 20:20:27 +0100

    Start of the universe?

---
+++ SolarSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Or is it solar system?");
+
+Engine e;
+
+Setup();
+Draw();
+
+void Setup()
+{
+    Raylib.InitWindow(800, 800, "Solar System!");
+    e = new();
+}
+
+void Draw()
+{
+    e.Run();
+}
